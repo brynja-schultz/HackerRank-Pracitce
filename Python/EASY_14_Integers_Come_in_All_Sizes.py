7113419fbba6616c295de6f75202c5f127d190d6
Brynja Schultz <54891716+brynja-schultz@users.noreply.github.com>
2025-08-20 14:49:32 +0000

    Delete Python directory

--- Python/EASY_14_Integers_Come_in_All_Sizes.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-
-result = (a ** b) + (c ** d)
-print(result)
